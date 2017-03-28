0de77e99090b42abd5aa12dc6e0e6938760d885c
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-28 09:51:45 +0900

    create new and index component

---
+++ imports/ui/NewQuestion.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class NewQuestion extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Hi! I am New Question!</h2>
+      </div>
+    );
+  }
+}
+
---
+++ imports/ui/QuestionIndex.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class QuestionIndex extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Hi! I am Question Index!</h2>
+      </div>
+    );
+  }
+}
