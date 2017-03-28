5e9105916435bb54b2d14657d98649d633a5dc69
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-28 09:39:59 +0900

    create nav links

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavLink from './NavLink'
 
 export default class App extends Component {
   render() {
@@ -6,7 +7,12 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>React Meteor Questions</h1>
+          <ul role="nav">
+            <li><NavLink to="/" onlyActiveOnIndex>Questions</NavLink></li>
+            <li><NavLink to="/new">Create Question</NavLink></li>
+          </ul>
         </header>
+        {this.props.children}
       </div>
     );
   }
