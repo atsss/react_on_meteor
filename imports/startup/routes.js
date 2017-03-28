977b123c3f56349948131f37697d1e0b9550c388
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-28 09:52:04 +0900

    set up routing

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 // route components
 import App from '../ui/App';
+import QuestionIndex from '../ui/QuestionIndex';
+import NewQuestion from '../ui/NewQuestion';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={QuestionIndex}/>
+      <Route path="/new" component={NewQuestion}/>
     </Route>
   </Router>
 );
