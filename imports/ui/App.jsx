18f586a62a7eb362969dc895d821727a33f05413
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-28 09:04:24 +0900

    create first view

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>React Meteor Questions</h1>
+        </header>
+      </div>
+    );
+  }
+}
