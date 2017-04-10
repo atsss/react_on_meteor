38d93c811be86a2421ba179e1acd45ac2c4cd4b0
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-04-10 19:48:01 +0900

    fix reload bug

--- imports/ui/QuestionDetail.jsx
+++ imports/ui/QuestionDetail.jsx
@@ -5,7 +5,6 @@ import { Questions } from '../api/questions.js';
 
 class QuestionDetail extends Component {
   render() {
-    console.log(this.props.question, 'component question');
     return (
       <div className="container">
         <h2>Hi! I am Question Detail!</h2>
@@ -21,8 +20,7 @@ QuestionDetail.propTypes = {
 };
 
 export default createContainer(({ params }) => {
-  console.log(params, 'createcontainer params');
   return {
-    question: Questions.findOne(params.id),
+    question: Questions.findOne(params.id) || {},
   };
 }, QuestionDetail);
