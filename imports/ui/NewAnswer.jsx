3eee6056ae33b46121dedbf17d6fb310b37bcdf0
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-04-11 21:54:32 +0900

    create new answer function

--- imports/ui/NewAnswer.jsx
+++ imports/ui/NewAnswer.jsx
@@ -1,22 +1,36 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
+import { Answers } from '../api/answers.js';
+
 export default class NewAnswer extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const content = ReactDOM.findDOMNode(this.refs.textContent).value.trim();
+
+    Answers.insert({
+      content,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textContent).value = '';
+  }
+
   render() {
     return (
       <div className="container">
         <h2>Hi! I am New Answer!</h2>
+
+        <form onSubmit={this.handleSubmit.bind(this)} >
+          <input
+            type="text"
+            ref="textContent"
+            placeholder="Type to add new answer"
+          />
+        </form>
       </div>
     );
   }
 }
-
-//QuestionIndex.propTypes = {
-//  questions: PropTypes.array.isRequired,
-//};
-//
-//export default createContainer(() => {
-//  return {
-//    questions: Questions.find({ content: { $exists: true }}).fetch(),
-//  };
-//}, QuestionIndex);
